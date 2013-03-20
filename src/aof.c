420759646cf44b51799208b0e77682b6b09c9433
Riceball LEE <snowyu.lee@gmail.com>
2013-03-20 11:03:56 +0800

    + Background append only file rewriting scheduled when iDB async writing...

--- src/aof.c
+++ src/aof.c
@@ -1003,7 +1003,7 @@ int rewriteAppendOnlyFileBackground(void) {
 void bgrewriteaofCommand(redisClient *c) {
     if (server.aof_child_pid != -1) {
         addReplyError(c,"Background append only file rewriting already in progress");
-    } else if (server.rdb_child_pid != -1) {
+    } else if (server.rdb_child_pid != -1 || server.idb_child_pid != -1) {
         server.aof_rewrite_scheduled = 1;
         addReplyStatus(c,"Background append only file rewriting scheduled");
     } else if (rewriteAppendOnlyFileBackground() == REDIS_OK) {
