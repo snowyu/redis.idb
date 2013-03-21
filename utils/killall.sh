f7783fa02cbae54edb8b4f98b953a72ac946f0d6
Riceball LEE <snowyu.lee@gmail.com>
2013-03-21 10:02:41 +0800

    + utils/killall to kill all redis server

---
+++ utils/killall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pids=`mktemp killredis`
+
+#ps ax|grep redis-server|tr -s " "|cut -d " " -f 1 > $pids
+ps ax|grep redis-server|awk '{print "kill -9 ", $1}' > $pids
+ps ax|grep redis-idb-bgsave|awk '{print "kill -9 ", $1}' >> $pids
+bash $pids
+rm $pids
