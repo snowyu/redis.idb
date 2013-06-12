0afc45b9ee5b8c05cf7b2f72d0efab250595af5c
Riceball LEE <snowyu.lee@gmail.com>
2013-06-12 20:36:14 +0800

    * update utils/create_ram_disk.sh

--- utils/create_ram_disk.sh
+++ utils/create_ram_disk.sh
@@ -11,5 +11,9 @@ echo the disk is $RAM_DISK, the  mount point is $MOUNT_POINT
 newfs_hfs $RAM_DISK
 mount -t hfs $RAM_DISK $MOUNT_POINT
 
+echo
+echo umount $MOUNT_POINT
+echo hdiutil eject $RAM_DISK
+
 #umount $MOUNT_POINT
 #hdiutil eject $RAM_DISK
