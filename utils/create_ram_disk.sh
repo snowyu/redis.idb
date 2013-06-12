740f3f83994ba1e1bdba6028a6d5984ea3e91e79
Riceball LEE <snowyu.lee@gmail.com>
2013-06-12 20:32:49 +0800

    + utils/create_ram_disk.sh for tests/tmp

---
+++ utils/create_ram_disk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+RAM_DISK=`hdiutil attach -nomount ram://7242880`
+MOUNT_POINT=$1
+
+echo the disk is $RAM_DISK, the  mount point is $MOUNT_POINT
+
+newfs_hfs $RAM_DISK
+mount -t hfs $RAM_DISK $MOUNT_POINT
+
+#umount $MOUNT_POINT
+#hdiutil eject $RAM_DISK
