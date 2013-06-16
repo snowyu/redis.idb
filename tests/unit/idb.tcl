8e24f216d33a9388512e1f6cdbf80baa11aa3cbc
Riceball LEE <snowyu.lee@gmail.com>
2013-06-16 16:44:52 +0800

    *[test] more test on subkeys

--- tests/unit/idb.tcl
+++ tests/unit/idb.tcl
@@ -1,11 +1,20 @@
 start_server {tags {"idb"}} {
 
     test {SUBKEYS to get all keys} {
-        foreach key {key_x key_y key_z} {
+        foreach key {key_x key_y key_z foo_a foo_b foo_c} {
             r set $key hello
         }
         r save
         lsort [r subkeys]
-    } {key_x key_y key_z}
+    } {foo_a foo_b foo_c key_x key_y key_z}
 
+    test {SUBKEYS to match foo* keys} {
+        lsort [r subkeys "." "foo*"]
+    } {foo_a foo_b foo_c}
+    test {SUBKEYS to get first page} {
+        lsort [r subkeys "" "" 3]
+    } {foo_a foo_b foo_c}
+    test {SUBKEYS to get second page} {
+        lsort [r subkeys "" "" 3 1*3]
+    } {key_x key_y key_z}
 }
